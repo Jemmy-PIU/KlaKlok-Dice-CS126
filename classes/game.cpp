ad969d828c33f004b6e9f7a758890abfd22a62b0
Jemmy <jemmy@Jemmys-M2.local>
2024-06-29 18:20:19 +0700

    New Branch

--- classes/game.cpp
+++ classes/game.cpp
@@ -115,6 +115,136 @@ public:
         }
         styleMessage.footerMsg("All Players Balance");
     };
+
+    // Check if a player exceeds 10 times the starting balance
+    bool isPlayerExceedsLimit()
+    {
+        for (int i = 0; i < totalPlayers; i++)
+        {
+            if (players[i].getBalance() > (startingBalance * 10))
+            {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    // Check if only one player is left has balance
+    bool isOnePlayerLeft()
+    {
+        int count = 0;
+        for (int i = 0; i < totalPlayers; i++)
+        {
+            if (players[i].getBalance() > 0)
+            {
+                count++;
+            }
+        }
+        return count == 1;
+    };
+
+    // Player turns
+    void playerTurns()
+    {
+        for (int i = 0; i < this->totalPlayers; i++)
+        {
+            Player &currentPlayer = this->players[i];
+
+            // Check if player is betting
+            cout << endl
+                 << "=====> Player " << i + 1 << "'s turn <=====" << endl;
+            currentPlayer.setIsTurn(true);
+            while (currentPlayer.getIsTurn() == true)
+            {
+                // Get input from 1-6
+                int input;
+                bool validInput = false;
+                while (!validInput)
+                {
+                    cout << "Enter a number from 1 to 6: ";
+                    cin >> input;
+                    // Check if input is valid
+                    if (cin.fail() || input < 1 || input > 6)
+                    {
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        cout << "Invalid input! Please enter a number from 1 to 6." << endl;
+                    }
+
+                    // Check if player already guessed the number
+                    else if (currentPlayer.inputExists(input))
+                    {
+                        cout << "You already guessed this number! Please enter a different number." << endl;
+                    }
+                    else
+                    {
+                        validInput = true;
+                    }
+                }
+                currentPlayer.addInputValues(input);
+
+                // Get bet amount
+                double betAmount;
+                validInput = false;
+                while (!validInput)
+                {
+                    cout << "Enter bet amount: ";
+                    cin >> betAmount;
+                    // Check if input is valid
+                    if (cin.fail() || betAmount <= 0)
+                    {
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        cout << "Invalid input! Please enter a valid bet amount." << endl;
+                    }
+                    else if (betAmount > currentPlayer.getBalance())
+                    {
+                        cout << "Bet amount should be less than or equal to your balance!" << endl;
+                    }
+                    else if (betAmount > currentPlayer.getCanBetAmount())
+                    {
+                        cout << "Can not bet! You only have " << currentPlayer.getCanBetAmount() << " left!" << endl;
+                    }
+                    else
+                    {
+                        validInput = true;
+                    }
+                }
+                currentPlayer.addBetAmounts(betAmount);
+
+                if (currentPlayer.getCanBetAmount() > 0 && currentPlayer.getInputValues().size() < 6)
+                {
+                    // Continue betting
+                    char continueBetting;
+                    validInput = false;
+                    while (!validInput)
+                    {
+                        cout << "Continue betting? (y/n): ";
+                        cin >> continueBetting;
+                        // Check if input is valid
+                        if (cin.fail() || (continueBetting != 'y' && continueBetting != 'n'))
+                        {
+                            cin.clear();
+                            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                            cout << "Invalid input! Please enter y or n." << endl;
+                        }
+                        else
+                        {
+                            validInput = true;
+                        }
+                    }
+
+                    // Set isBetting
+                    bool isTurn = (continueBetting == 'y') ? true : false;
+                    currentPlayer.setIsTurn(isTurn);
+                }
+                else
+                {
+                    currentPlayer.setIsTurn(false);
+                }
+            }
+        }
+    }
 };
 
 class StartGame : public Game
@@ -141,5 +271,8 @@ public:
 
         // Start the game
         cout << "Game started!" << endl;
+
+        // Player turns
+        game.playerTurns();
     }
 };
